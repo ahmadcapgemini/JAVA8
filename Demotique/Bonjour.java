e910ee7d5211c4ae4b059e9c9673491763a895d0
Ahmad <jahmad@hotmail.fr>
2016-06-20 16:15:00 +0200

    1

---
+++ Demotique/Bonjour.java
@@ -0,0 +1,53 @@
+public class Bonjour {
+	
+		public static void main( String args[] ){
+			System.out.println("Bien le bonjour de toute la classe.");
+		}
+}
+
+
+
+
+
+
+
+public class Convertisseur {
+
+	public static void main(String[] args) {
+
+		//---------------------------------
+		//Conversion d'un string en entier
+		//---------------------------------
+
+		String strMaVariable = "22";
+		int iMaVariable;
+
+		
+		//Convertir strMaVariable en iMaVariable
+
+
+		//.......................................
+
+		//System.out.println("" + iMaVariable);
+
+
+
+		//---------------------------------
+		//Conversion d'un entier en String
+		//---------------------------------
+
+
+		int iMaVariable2 = 10;
+		String strMaVariable2;
+
+		//Convertir iMaVariable2 en strMaVariable2
+
+
+		//.......................................
+
+		//System.out.println("" + strMaVariable2);
+
+
+
+	}
+}
