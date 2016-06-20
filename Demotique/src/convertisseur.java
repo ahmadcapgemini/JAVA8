8104767cf9e641ba5b4cf4e2aa8fb1ae481a7696
Ahmad <jahmad@hotmail.fr>
2016-06-20 16:29:25 +0200

    1

---
+++ Demotique/src/convertisseur.java
@@ -0,0 +1,39 @@
+
+public class convertisseur {
+
+
+
+	public static void main( String args[] ){
+
+		String strMaVariable = "22";
+		
+
+
+		System.out.println(conversionEnInt(strMaVariable)+2);
+
+		System.out.println(conversionEnstring(conversionEnInt(strMaVariable))+2);
+		
+
+	}
+
+/**
+ * @param chaine de caractère
+ * @return Integer
+ */
+	private static int conversionEnInt(String chaine){
+		 int converteur = Integer.parseInt(chaine);
+		 return converteur;
+	}
+
+
+
+/**
+ * @param Integer
+ * @return String
+ */
+	private static String conversionEnstring(int number){
+		 String converteur = Integer.toString(number);
+		 return converteur;
+	}
+}
+
