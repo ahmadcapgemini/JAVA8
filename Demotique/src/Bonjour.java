50e953b3bd5b8d7000dec1fab750b7b8108efaaa
Ahmad <jahmad@hotmail.fr>
2016-06-21 09:03:16 +0200

    TP2 (Bonjour) &  TP3 (Convertisseur) résolus à 9h 17

--- Demotique/src/Bonjour.java
+++ Demotique/src/Bonjour.java
@@ -1,6 +1,6 @@
 public class Bonjour {
 	
-		public static void main( String args[] ){
+	public static void main(String args[]) {
 			System.out.println("Bien le bonjour de toute la classe.");
 		}
 }
--- Demotique/src/Convertisseur.java
+++ Demotique/src/Convertisseur.java
@@ -1,39 +1,32 @@
 
 public class Convertisseur {
 
-
-
-	public static void main( String args[] ){
+	public static void main(String args[]) {
 
 		String strMaVariable = "22";
-		
 
+		System.out.println(conversionEnInt(strMaVariable) + 2);
 
-		System.out.println(conversionEnInt(strMaVariable)+2);
-
-		System.out.println(conversionEnstring(conversionEnInt(strMaVariable))+2);
-		
+		System.out.println(conversionEnstring(conversionEnInt(strMaVariable)) + 2);
 
 	}
 
-/**
- * @param chaine de caractère
- * @return Integer
- */
-	private static int conversionEnInt(String chaine){
-		 int converteur = Integer.parseInt(chaine);
-		 return converteur;
+	/**
+	 * @param chaine
+	 *            de caractère
+	 * @return Integer
+	 */
+	private static int conversionEnInt(String chaine) {
+		int converteur = Integer.parseInt(chaine);
+		return converteur;
 	}
 
-
-
-/**
- * @param Integer
- * @return String
- */
-	private static String conversionEnstring(int number){
-		 String converteur = Integer.toString(number);
-		 return converteur;
+	/**
+	 * @param Integer
+	 * @return String
+	 */
+	private static String conversionEnstring(int number) {
+		String converteur = Integer.toString(number);
+		return converteur;
 	}
 }
-
